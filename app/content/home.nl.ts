1da1472a3aa742371bcb312f36953fd9365302c3
Thunnisvanoort <43135614+Thunnisvanoort@users.noreply.github.com>
2026-01-06 13:47:03 +0100

    Update home.nl.ts
    further text edits

--- app/content/home.nl.ts
+++ app/content/home.nl.ts
@@ -14,14 +14,13 @@ export const homeContent: HomeContent = {
   ui: {
     navigation: {
       locationLabel: 'Paramaribo • Suriname',
-      projectCode: 'STM',
-      projectSubtitle: 'Suriname Tijdachine',
+      projectCode: 'Suriname Tijdmachine',
       languageToggleLabel: 'Nederlands',
     },
     section01Hero: {
-      tagline: 'Suriname Tijdmachine',
+      tagline: 'Huygens Instituut | Historische Database Suriname en de Cariben | Stichting Pica | KNAW Citizen Science Incubator',
       title: 'De Suriname Tijdmachine brengt verspreide historische bronnen samen op de kaart.',
-      lead: 'Gebruikers hebben alles op één plek, zonder telkens te moeten controleren of gegevens over dezelfde mensen, straten of plantages gaan. Zo wordt het gemakkelijker om de weg te vinden in het verleden.',
+      lead: 'Gebruikers vinden alle informatie op één plek, zonder telkens te moeten controleren of gegevens over dezelfde mensen, straten of plantages gaan. Zo wordt het gemakkelijker om de weg te vinden in het verleden. De herkomst van informatie wordt altijd vermeld zodat gebruikers zelf de originele bron kunnen raadplegen en hun eigen oordeel vellen. ',
       kicker:
         'De Suriname Tijdmachine is een samenwerkingsverband van burgerwetenschappers, erfgoedinstellingen en onderzoekers.',
       primaryCtaLabel: 'Ontdek de Tijdmachine',
